c173825397e24ec002473ad302bcaa30287be16c
Luichang <charlesbarbret@yahoo.com>
2018-10-26 14:04:04 +0200

    Added changes to satisfy checkstyle

--- Swap.java
+++ Swap.java
@@ -1,46 +1,62 @@
 public class Swap {
-    private int id;
-    private java.util.Date date;
-    private static int nextID = 1;
-    private Trainer trainer1;
-    private Trainer trainer2;
-    private Pokemon pokemon1;
-    private Pokemon pokemon2;
-        
-    public Swap(Trainer trainer1, Trainer trainer2, Pokemon pokemon1, Pokemon pokemon2) {
-        this.date = new java.util.Date();
-        this.id = Swap.nextID;
-        Swap.nextID++;
-        this.trainer1 = trainer1;
-        this.trainer2 = trainer2;
-        this.pokemon1 = pokemon1;
-        this.pokemon2 = pokemon2;
+  //private int id;
+  private String id;
+  private java.util.Date date;
+  //private static int nextID = 1;
+  private Trainer trainer1;
+  private Trainer trainer2;
+  private Pokemon pokemon1;
+  private Pokemon pokemon2;
+  
+  /** Used to trade two Pokemon between two different Trainer. */
+  public Swap(Trainer trainer1, Trainer trainer2, Pokemon pokemon1, Pokemon pokemon2) {
+    this.date = new java.util.Date();
+    //this.id = Swap.nextID;
+    //Swap.nextID++;
+    this.trainer1 = trainer1;
+    this.trainer2 = trainer2;
+    this.pokemon1 = pokemon1;
+    this.pokemon2 = pokemon2;
+    
+    /* using this id as the assignment wants us to create a string ID and this seems to be
+     * the only option to have a string, use the date and try to be unique. In our (Jasmin and 
+     * Charles) mind it seems more practical to use an incrementing int as we provided in comments 
+     * above, however for the sake of the assignment this is what we came up with  
+     */
+    
+    this.id = this.date.toString() + this.trainer1.toString() + this.trainer2.toString() 
+      + this.pokemon1.toString() + this.pokemon2.toString();
+  }
+  
+  /* this function is added to please Checkstyle as it is not required in any tests or other 
+   * functions
+   */
+  public String getid() {
+    return this.id;
+  }
+  
+  /** this function allows for a change in permissions before the trade happens, 
+   also makes the constructor cleaner. */
+  public void execute() {
+    if (this.pokemon1.getSwapAllow() && this.pokemon2.getSwapAllow()) {
+      if (trainer1 != trainer2) {
+        this.pokemon1.trade(this, trainer2);
+        this.pokemon2.trade(this, trainer1);
+        this.trainer1.trade(this.pokemon1, this.pokemon2);
+        this.trainer2.trade(this.pokemon2, this.pokemon1);
+        System.out.println("Trade was successful");
+      } else {
+        System.out.println("ERROR: You can't trade with yourself");
+      }
+    } else {
+      if (this.pokemon2.getSwapAllow()) {
+        System.out.println("ERROR: Pokemon " + this.pokemon1.getName() + " is not up for trade");
+      } else if (this.pokemon1.getSwapAllow()) {
+        System.out.println("ERROR: Pokemon " + this.pokemon2.getName() + " is not up for trade");
+      } else {
+        System.out.println("ERROR: Both Pokemon " + this.pokemon1.getName() 
+                + " and " + this.pokemon2.getName() + " is not up for trade");
+      }
     }
-        
-    public void execute() {
-        if (this.pokemon1.getSwapAllow() && this.pokemon2.getSwapAllow()) {
-            if (trainer1 != trainer2) {
-                this.pokemon1.trade(this, trainer2);
-                this.pokemon2.trade(this, trainer1);
-                this.trainer1.trade(this.pokemon1, this.pokemon2);
-                this.trainer2.trade(this.pokemon2, this.pokemon1);
-                System.out.println("Trade was successful");
-            }
-            else {
-                System.out.println("ERROR: You can't trade with yourself");
-            }
-        }
-        else {
-            if (this.pokemon2.getSwapAllow()) {
-                System.out.println("ERROR: Pokemon " + this.pokemon1.getName() + " is not up for trade");
-            }
-            else if (this.pokemon1.getSwapAllow()) {
-                System.out.println("ERROR: Pokemon " + this.pokemon2.getName() + " is not up for trade");
-            } 
-            else {
-                System.out.println("ERROR: Both Pokemon " + this.pokemon1.getName() 
-                                + " and " + this.pokemon2.getName() + " is not up for trade");
-            }
-        }
-    }
+  }
 }
--- Trainer.java
+++ Trainer.java
@@ -1,77 +1,86 @@
 public class Trainer {
-    private String firstname;
-    private String lastname;
-    private java.util.ArrayList<Pokemon> pokemons;
-        
-        //Konstruktor
-        
-    public Trainer(String firstname, String lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        pokemons = new java.util.ArrayList<Pokemon>();
+  private String firstname;
+  private String lastname;
+  private java.util.ArrayList<Pokemon> pokemons;
+    
+  //Konstruktor
+  
+  /** Creates a new Trainer with entered first and last name. */
+  public Trainer(String firstname, String lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    pokemons = new java.util.ArrayList<Pokemon>();
+  }
+    
+  //Getter-Methoden
+  
+  public String getFirstname() {
+    return this.firstname;
+  }
+    
+  public String getLastname() {
+    return this.lastname;
+  }
+  
+  /** Depending on Parameter you get a list of all Pokemon you own, all of a Type you 
+   * own or a specific one, based on the index. */
+  public java.util.ArrayList<String> getPokemons() {
+    java.util.ArrayList<String> out = new java.util.ArrayList<String>();
+    for (int i = 0; i < this.pokemons.size(); i++) {
+      out.add(pokemons.get(i).toString());
     }
-        
-        //Getter-Methoden
-    public String getFirstname() {
-        return this.firstname;
+    return out;
+  }
+  
+  /** Depending on Parameter you get a list of all Pokemon you own, all of a Type you 
+   * own or a specific one, based on the index. */
+  public java.util.ArrayList<String> getPokemons(Type type) {
+    java.util.ArrayList<String> out = new java.util.ArrayList<String>();
+    for (int i = 0; i < this.pokemons.size(); i++) {
+      if (pokemons.get(i).getType() == type) {
+        out.add(pokemons.get(i).toString());
+      }
     }
-        
-    public String getLastname() {
-        return this.lastname;
+    return out;
+  }
+  
+  /** Depending on Parameter you get a list of all Pokemon you own, all of a Type you 
+   * own or a specific one, based on the index. */
+  public String getPokemons(int index) {
+    for (int i = 0; i < this.pokemons.size(); i++) {
+      if (index == pokemons.get(i).getNumber()) {
+        return pokemons.get(i).toString();
+      }
     }
+    return "You don't have access to this pokemon";
         
-    public java.util.ArrayList<String> getPokemons() {
-        java.util.ArrayList<String> out = new java.util.ArrayList<String>();
-        for (int i = 0; i < this.pokemons.size(); i++) {
-            out.add(pokemons.get(i).toString());
-        }
-        return out;
+  }
+  //Setter-Methoden
+    
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+    
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+    
+  public String toString() {
+    return (this.firstname + " " + this.lastname);
+  }
+    
+  //Sonstige Funktionen
+  
+  /** Catching a Pokemon adds it to your list that you own. */
+  public void addPokemon(Pokemon pokemon) {
+    if (!pokemon.hasTrainer()) {
+      this.pokemons.add(pokemon);
+      pokemon.addTrainer(this);
     }
-        
-    public java.util.ArrayList<String> getPokemons(Type type) {
-        java.util.ArrayList<String> out = new java.util.ArrayList<String>();
-        for (int i = 0; i < this.pokemons.size(); i++) {
-            if (pokemons.get(i).getType() == type) {
-                out.add(pokemons.get(i).toString());
-            }
-        }
-        return out;
-    }
-
-    public String getPokemons(int index) {
-        for (int i = 0; i < this.pokemons.size(); i++) {
-            if (index == pokemons.get(i).getNumber()) {
-                return pokemons.get(i).toString();
-            }
-        }
-        return "You don't have access to this pokemon";
-                
-    }
-        //Setter-Methoden
-        
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-        
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-        
-    public String toString() {
-        return (this.firstname + " " + this.lastname);
-    }
-        
-        //Sonstige Funktionen
-        
-    public void addPokemon(Pokemon pokemon) {
-        if (!pokemon.hasTrainer()) {
-            this.pokemons.add(pokemon);
-            pokemon.addTrainer(this);
-        }
-    }
-        
-    public void trade(Pokemon own, Pokemon other) {
-        pokemons.remove(own);
-        this.pokemons.add(other);
-    }
+  }
+    
+  public void trade(Pokemon own, Pokemon other) {
+    pokemons.remove(own);
+    this.pokemons.add(other);
+  }
 }
